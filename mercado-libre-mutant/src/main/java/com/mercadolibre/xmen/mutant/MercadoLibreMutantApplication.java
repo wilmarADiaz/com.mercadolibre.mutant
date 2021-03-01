ca8910b0df97bf26f1c13ff2f2f9e6b8acb8896b
wilmar <wilmar@wilmar-lm>
2021-02-28 22:29:23 -0500

    Primer Commit

---
+++ mercado-libre-mutant/src/main/java/com/mercadolibre/xmen/mutant/MercadoLibreMutantApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.xmen.mutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLibreMutantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLibreMutantApplication.class, args);
+	}
+
+}
---
+++ mercado-libre-mutant/src/test/java/com/mercadolibre/xmen/mutant/MercadoLibreMutantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.xmen.mutant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLibreMutantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
